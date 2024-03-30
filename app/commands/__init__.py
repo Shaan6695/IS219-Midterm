389b1abdf8fc80f83498dc574fbb06e2cc4e80da
Shaan6695 <sam33@njit.edu>
2024-03-29 23:46:21 -0400

    starting to get the logging to work

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -2,25 +2,34 @@ from abc import ABC, abstractmethod
 
 class Command(ABC):
     @abstractmethod
-    def execute(self):
+    def execute(self, *args):
         pass
 
+class HistoryCommand(Command):
+    def __init__(self, app_instance):
+        self.app_instance = app_instance
+
+    def execute(self, *args):
+        self.app_instance.display_history()
+
+# Modify CommandHandler to handle 'history' command
 class CommandHandler:
     def __init__(self):
         self.commands = {}
+        self.history = []  # Initialize history as an empty list
 
-    def register_command(self, command_name: str, command: Command):
-        self.commands[command_name] = command
+    def register_command(self, command_name, command_instance):
+        self.commands[command_name] = command_instance
 
-    def execute_command(self, command_name: str):
-        """ Look before you leap (LBYL) - Use when its less likely to work
-        if command_name in self.commands:
-            self.commands[command_name].execute()
+    def execute_command(self, command_name, *args):
+        command_instance = self.commands.get(command_name)
+        if command_instance:
+            return command_instance.execute(*args)
         else:
             print(f"No such command: {command_name}")
-        """
-        """Easier to ask for forgiveness than permission (EAFP) - Use when its going to most likely work"""
-        try:
-            self.commands[command_name].execute()
-        except KeyError:
-            print(f"No such command: {command_name}")
+
+    def add_to_history(self, calculation):
+        self.history.append(calculation)  # Add calculation to history list
+
+    def get_history(self):
+        return self.history  # Return the history list
---
+++ readme.md
@@ -0,0 +1,1 @@
+IS219 Midterm Under Consctruction
