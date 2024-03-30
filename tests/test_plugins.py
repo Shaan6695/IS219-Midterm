e2e67660ac5b354ade026dd54d0b314363ac422d
Shaan6695 <sam33@njit.edu>
2024-03-29 23:36:38 -0400

    updating tests

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,19 +1,38 @@
-import pytest
-from app import App
+"""Test file for plugins"""
+
+from app.plugins.discord import DiscordCommand
+from app.plugins.email import EmailCommand
 from app.plugins.goodbye import GoodbyeCommand
 from app.plugins.greet import GreetCommand
 
-def test_greet_command(capfd):
-    command = GreetCommand()
-    command.execute()
-    out, err = capfd.readouterr()
-    assert out == "Hello, World!\n", "The GreetCommand should print 'Hello, World!'"
 
-def test_goodbye_command(capfd):
-    command = GoodbyeCommand()
-    command.execute()
-    out, err = capfd.readouterr()
-    assert out == "Goodbye\n", "The GreetCommand should print 'Hello, World!'"
+def test_email_command(capsys):
+    """Test EmailCommand execution"""
+    email_command = EmailCommand()
+    email_command.execute()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "I will email you"
 
+def test_discord_command(capsys):
+    """Test DiscordCommand execution"""
+    discord_command = DiscordCommand()
+    discord_command.execute()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "I will send something on discord"
 
+def test_greet_command(capsys):
+    """Test GreetCommand execution"""
+    greet_command = GreetCommand()
+    greet_command.execute()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Hello, World!"
+    
 
+def test_goodbye_command(capsys):
+    """Test GoodbyeCommand execution"""
+    goodbye_command = GoodbyeCommand()
+    goodbye_command.execute()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Goodbye"
+
+    
